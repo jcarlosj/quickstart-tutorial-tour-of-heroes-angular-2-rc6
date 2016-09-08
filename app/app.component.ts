40952f182177b3b8eaed2d3c3a1eabf27773029b
Juan Carlos Jiménez Gutiérrez <jcarlosj@developer>
2016-09-08 11:30:58 -0500

    Agrega : Clase e instancia para el despliegue de datos

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,30 @@
 // Imports del core de Angular 2 necesarios para este componente
 import { Component } from '@angular/core';
 
+// Clase Hero
+export class Hero {
+  id: number;
+  name: string;
+}
+
 // Decorador
 @Component({
   selector: 'my-app',
-  template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>'
+  template:`
+  <h1>{{title}}</h1>
+  <h2>{{hero.name}} details!</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div><label>name: </label>{{hero.name}}</div>
+  `
+
 })
 
 // Clase principal del componente
 export class AppComponent {
+  // Atributos
   title = 'Tour of Heroes';
-  hero = 'Windstorm';
+  hero: Hero = { // <-- Instanciamos la clase Hero
+    id: 1,
+    name: 'Windstorm'
+  };
 }
