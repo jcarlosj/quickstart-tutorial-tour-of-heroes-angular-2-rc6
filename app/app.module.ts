c4397c62e4444d8e61ac737d5b245798d1e1b07f
Juan Carlos Jiménez Gutiérrez <jcarlosj@developer>
2016-09-08 11:45:46 -0500

    hero : agrega formulario a la vista

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,17 @@
 // Imports del core de Angular 2 necesarios para este componente
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 
 // Imports personalizados necesarios para este componente
 import { AppComponent }   from './app.component';
 
 // Decorador
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ 
+    BrowserModule,
+    FormsModule 
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
