28751e9ce1f410c48f93ff197190e3f0cdf3768a
Juan Carlos Jiménez Gutiérrez <jcarlosj@developer>
2016-09-08 12:34:13 -0500

    hero : despliega listado a traves de un Array de datos

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,17 +7,30 @@ export class Hero {
   name: string;
 }
 
+// Array de datos constantes
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 // Decorador
 @Component({
   selector: 'my-app',
   template:`
-    <h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="let hero of heroes">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
   `
 })
 
@@ -25,8 +38,5 @@ export class Hero {
 export class AppComponent {
   // Atributos
   title = 'Tour of Heroes';
-  hero: Hero = { // <-- Instanciamos la clase Hero
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
 }
