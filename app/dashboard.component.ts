03c251a587f1a7ddc633149c7fdb11fde0965189
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2016-09-09 11:54:38 -0500

    hero : desplegar del listado de heroes (2do, 3ro, 4to, 5to) en el componente inicial

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,11 +1,28 @@
 // Imports del core de Angular 2 necesarios para este componente
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Imports personalizados necesarios para este componente
+import { Hero } from './hero';
+import { HeroService } from './hero.service';
 
 // Decorador
 @Component({
   selector: 'my-dashboard',
-  template: '<h3>My Dashboard</h3>'
+  templateUrl: 'app/dashboard.component.html'
 })
 
 // Clase principal del componente
-export class DashboardComponent { }
+export class DashboardComponent implements OnInit { 
+      heroes: Hero[] = [];
+
+  // Constructor    
+  constructor( private heroService: HeroService ) { }
+
+  // Lifecycle Hooks
+  ngOnInit(): void {
+    this.heroService.getHeroes()
+        .then( heroes => this.heroes = heroes.slice( 1, 5 ) );
+  }
+
+  gotoDetail(hero: Hero): void { /* no implementado aun */}
+}
